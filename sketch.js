385813e21a8fc69ab4eab37713f84d44da211fa8
Pieter de Marez Oyens <pdemarezoyens@pp.studio>
2026-02-09 13:41:06 +0100

    updated sketch and css

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 let scrollTarget01 = 0; // raw scroll, 0..1
 let scrollSmooth01 = 0; // smoothed scroll, 0..1
+const num_circles = 500;
+const min_diameter = 10;
 
 function updateScrollTarget() {
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -23,20 +25,24 @@ function draw() {
   background(8, 10, 18);
 
   // 0.08–0.2 is a good range. Higher = snappier, lower = smoother.
-  const ease = 0.08;
+  const ease = 0.01;
   scrollSmooth01 = lerp(scrollSmooth01, scrollTarget01, ease);
 
   // Use smoothed scroll to drive noise "time"
-  const t = scrollSmooth01 * 1.0; // scale controls how far positions travel
+  const t = scrollSmooth01 * 0.5; // scale controls how far positions travel
+  draw_circles(t, num_circles)
 
-  for (let i = 0; i < 30; i++) {
+}
+
+function draw_circles(t, num_circles) {
+  for (let i = 0; i < num_circles; i++) {
     const nx = noise(i * 10, t);
     const ny = noise(i * 10 + 1000, t);
 
     const x = nx * width;
     const y = ny * height;
 
-    const r = 160 + 120 * noise(i * 10 + 2000, t);
+    const r = min_diameter + 120 * noise(i * 10 + 2000, t);
     fill(120 + 80 * nx, 90 + 80 * ny, 200, 22);
     circle(x, y, r);
   }
